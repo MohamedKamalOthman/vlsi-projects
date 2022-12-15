3a5e35dca11ce3dd8ac5505aeb945212ed3d7c45
Mohammed Abdulhady <m.abdulhady00@gmail.com>
2022-12-15 15:12:04 +0200

    fix: subnormal issue in floating mult

--- mini_project_2/rtl/floating.v
+++ mini_project_2/rtl/floating.v
@@ -5,7 +5,6 @@ module floating (
     output reg [31:0] o_res
 );
   reg [31:0] a, b;
-  wire [7:0] Ea, Eb;
   wire Sa, Sb;
   assign Sa = a[31];
   assign Sb = b[31];
@@ -23,30 +22,47 @@ module floating (
       .enable(enable)
   );
 
-  wire [23:0] Na, Nb;
+  wire [31:0] inA, inB;
+  wire aSubn, bSubn;
+  assign aSubn = (outA == 3'b001);
+  assign bSubn = (outB == 3'b001);
+  assign inA   = {a[30:24], a[23] | aSubn, !aSubn, a[22:0]};
+  assign inB   = {b[30:24], b[23] | bSubn, !bSubn, b[22:0]};
 
-  // add extra bit
-  assign Na = {outA != 3'b001, a[22:0]};
-  assign Nb = {outB != 3'b001, b[22:0]};
-  assign Ea = a[30:23];
-  assign Eb = b[30:23];
+  wire [7:0] Ea, Eb;
+
+  wire [31:0] subn;
+  assign subn = (aSubn) ? inA : inB;
+
+  wire [4:0] shamt;
+  zero_counter zcn (
+      subn[23:0],
+      shamt
+  );
+
+  wire zero;
+  wire [23:0] Na, Nb;
+  assign Na = (aSubn) ? inB[23:0] : inA[23:0];
+  assign Nb = subn[23:0] << shamt;
+
+  assign {zero, Ea} = ((aSubn) ? inB[31:24] : inA[31:24]) - shamt;
+  assign Eb = (aSubn) ? inA[31:24] : inB[31:24];
 
   wire [47:0] mult_res;
   wire [22:0] mult_shft;
   wire [ 7:0] E_res;
-  wire [8:0] E_sum, E_sub, E_offset, E_intermediate;
+  wire [8:0] E_sum, E_sub;
   wire [22:0] M_res;
+  wire underflow;
   assign mult_res = Na * Nb;
-  assign mult_shft = (mult_res[47]) ? mult_res[46:24] : (mult_res[46] | (Na[23] & Nb[23]))? mult_res[45:23] : mult_res[44:22];
-  assign E_intermediate = Ea + Eb;
-  assign E_offset = (Na[23] & Nb[23]) ? mult_res[47] : (mult_res[46])? 9'b1 : (mult_res[45])? 9'b0 : (E_intermediate != 9'b0)? -9'b1 : 9'b0;
-  assign E_sum = E_intermediate + E_offset;
-  assign E_sub = E_sum - 9'd127;
-  assign E_res = (E_sum < 9'd127) ? 8'b0 : (E_sub[8]) ? 8'hff : E_sub[7:0];
+  assign mult_shft = (mult_res[47]) ? mult_res[46:24] : mult_res[45:23];
+  assign E_sum = Ea + Eb + mult_res[47];
+  assign {underflow, E_sub} = {1'b0, E_sum} - 10'd127;
+  assign E_res = (underflow | zero) ? 8'b0 : (E_sub[8]) ? 8'hff : E_sub[7:0];
   // possible optimizations 2's complement Esub instead of 127 - Esum
   // check bits instead of comparator E_sum < 127
   // shift max is 24 bits otherwise 0
-  assign M_res = (E_res == 8'hff) ? 23'b0 : ((E_sum < 9'd127) & (E_res == 8'h00))? mult_shft >> (9'd127 - E_sum + (Na[23] & Nb[23])): mult_shft;
+  assign M_res = (E_res == 8'hff | zero) ? 23'b0 : (E_res == 8'h00)? (mult_res[46:23] >> (8'd128 - E_sum[7:0])) : mult_shft;
   assign float_res = {Sa ^ Sb, E_res, M_res};
 
   assign res = (enable) ? float_res : special_res;
@@ -140,5 +156,39 @@ module n_case (
 
   /// concatinating the wires to the real output.
   assign S = {SS, ES, MS};
+
+endmodule
+
+
+module zero_counter (
+    input  [23:0] M,
+    output [ 4:0] Zcount
+);
+  wire [23:0] Z;
+  assign Z = 24'b0;
+  assign Zcount =  M[23:0]  == Z[23:0] ? 5'h18 : 
+				 M[23:1]  == Z[23:1] ? 5'h17 :
+				 M[23:2]  == Z[23:2] ? 5'h16 :
+				 M[23:3]  == Z[23:3] ? 5'h15 :
+				 M[23:4]  == Z[23:4] ? 5'h14 :
+				 M[23:5]  == Z[23:5] ? 5'h13 :
+				 M[23:6]  == Z[23:6] ? 5'h12 :
+				 M[23:7]  == Z[23:7] ? 5'h11 :
+				 M[23:8]  == Z[23:8] ? 5'h10 :
+				 M[23:9]  == Z[23:9] ? 5'hf :
+				 M[23:10] == Z[23:10] ? 5'he :
+				 M[23:11] == Z[23:11] ? 5'hd :
+				 M[23:12] == Z[23:12] ? 5'hc :
+				 M[23:13] == Z[23:13] ? 5'hb :
+				 M[23:14] == Z[23:14] ? 5'ha :
+				 M[23:15] == Z[23:15] ? 5'h9 :
+				 M[23:16] == Z[23:16] ? 5'h8 :
+				 M[23:17] == Z[23:17] ? 5'h7 :
+				 M[23:18] == Z[23:18] ? 5'h6 :
+				 M[23:19] == Z[23:19] ? 5'h5 :
+				 M[23:20] == Z[23:20] ? 5'h4 :
+				 M[23:21] == Z[23:21] ? 5'h3 :
+				 M[23:22] == Z[23:22] ? 5'h2 :
+				 M[23]    == Z[23] ? 5'h1 : 5'h0;
 
 endmodule
